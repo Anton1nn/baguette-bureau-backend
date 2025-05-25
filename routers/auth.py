3eac100bfb3f92fa4d9379027e38754e0ded8368
Anton1nn <124528433+Anton1nn@users.noreply.github.com>
2025-05-25 12:44:24 +0200

    Update auth.py

--- routers/auth.py
+++ routers/auth.py
@@ -6,7 +6,6 @@ from models.user import User
 from sqlalchemy.future import select
 from fastapi import APIRouter
 from models.user import UserOut
-from schemas.user import UserOut
 from schemas import UserOut
 
 
